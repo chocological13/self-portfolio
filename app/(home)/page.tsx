01ca35c1c6904f92aea83a345deb7c516a11d0aa
chocological13 <155338364+chocological13@users.noreply.github.com>
2024-10-31 12:23:27 +0700

    feat: hero section

---
+++ app/(home)/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+import Hero from "@/app/(home)/components/Hero";
+
+export default function Home() {
+  return (
+    <div>
+      <main>
+        <Hero />
+      </main>
+    </div>
+  );
+}
